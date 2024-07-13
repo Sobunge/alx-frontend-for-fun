b935c0cc5ae9e62947ce08f2d67e70dfcd488c4d
Samuel Obunge <samuelobunge@yahoo.com>
2024-07-13 07:53:41 +0300

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,43 +1,108 @@
 #!/usr/bin/python3
-'''
-A script that codes markdown to HTML
-'''
+"""
+A script that converts Markdown to HTML.
+"""
+
 import sys
 import os
 import re
+import hashlib
+
+def convert_markdown_to_html(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as file_1:
+        html_content = []
+        md_content = [line.rstrip() for line in file_1]
+
+        in_unordered_list = False
+        in_ordered_list = False
+        in_paragraph = False
+
+        for line in md_content:
+            # Convert headings
+            heading_match = re.match(r'^(#{1,6}) (.*)', line)
+            if heading_match:
+                if in_unordered_list:
+                    html_content.append('</ul>\n')
+                    in_unordered_list = False
+                if in_ordered_list:
+                    html_content.append('</ol>\n')
+                    in_ordered_list = False
+                if in_paragraph:
+                    html_content.append('</p>\n')
+                    in_paragraph = False
+                h_level = len(heading_match.group(1))
+                html_content.append(f'<h{h_level}>{heading_match.group(2)}</h{h_level}>\n')
+            # Convert unordered lists
+            elif line.startswith('- '):
+                if in_ordered_list:
+                    html_content.append('</ol>\n')
+                    in_ordered_list = False
+                if not in_unordered_list:
+                    html_content.append('<ul>\n')
+                    in_unordered_list = True
+                item = line[2:]  # Remove '- ' from the start
+                html_content.append(f'    <li>{item}</li>\n')
+            # Convert ordered lists
+            elif line.startswith('* '):
+                if in_unordered_list:
+                    html_content.append('</ul>\n')
+                    in_unordered_list = False
+                if not in_ordered_list:
+                    html_content.append('<ol>\n')
+                    in_ordered_list = True
+                item = line[2:]  # Remove '* ' from the start
+                html_content.append(f'    <li>{item}</li>\n')
+            else:
+                if in_unordered_list:
+                    html_content.append('</ul>\n')
+                    in_unordered_list = False
+                if in_ordered_list:
+                    html_content.append('</ol>\n')
+                    in_ordered_list = False
+
+                # Handle special cases
+                line = re.sub(r'\[\[(.+?)\]\]', lambda match: hashlib.md5(match.group(1).encode()).hexdigest(), line)
+                line = re.sub(r'\(\((.+?)\)\)', lambda match: match.group(1).replace('c', '').replace('C', ''), line)
+                
+                # Convert paragraphs
+                if line:
+                    if not in_paragraph:
+                        html_content.append('<p>\n')
+                        in_paragraph = True
+                    # Handle bold and emphasized text within paragraphs
+                    line = re.sub(r'\*\*(.+?)\*\*', r'<b>\1</b>', line)
+                    line = re.sub(r'__(.+?)__', r'<em>\1</em>', line)
+                    html_content.append(f'    {line}<br />\n')
+                else:
+                    if in_paragraph:
+                        html_content.append('</p>\n')
+                        in_paragraph = False
+
+        # Close any remaining open tags
+        if in_unordered_list:
+            html_content.append('</ul>\n')
+        if in_ordered_list:
+            html_content.append('</ol>\n')
+        if in_paragraph:
+            html_content.append('</p>\n')
+
+    with open(output_file, 'w', encoding='utf-8') as file_2:
+        file_2.writelines(html_content)
 
 if __name__ == '__main__':
-
-    # Test that the number of arguments passed is 2
-    if len(sys.argv[1:]) != 2:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
+    # Check that the number of arguments passed is 2
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         sys.exit(1)
 
     # Store the arguments into variables
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    # Checks that the markdown file exists and is a file
+    # Check that the markdown file exists and is a file
     if not (os.path.exists(input_file) and os.path.isfile(input_file)):
         print(f'Missing {input_file}', file=sys.stderr)
         sys.exit(1)
 
-    with open(input_file, encoding='utf-8') as file_1:
-        html_content = []
-        md_content = [line[:-1] for line in file_1.readlines()]
-        for line in md_content:
-            heading = re.split(r'#{1,6} ', line)
-            if len(heading) > 1:
-                # Compute the number of the # present to
-                # determine heading level
-                h_level = len(line[:line.find(heading[1])-1])
-                # Append the html equivalent of the heading
-                html_content.append(
-                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
-                )
-            else:
-                html_content.append(line)
-
-    with open(output_file, 'w', encoding='utf-8') as file_2:
-        file_2.writelines(html_content)
+    convert_markdown_to_html(input_file, output_file)
+    sys.exit(0)
